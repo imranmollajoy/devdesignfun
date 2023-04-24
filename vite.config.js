218883cc535fec4bf03ba3668b6a07c7f4c042e0
Imran Molla Joy <imranmollajoy@gmail.com>
2023-04-24 16:20:29 +0600

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+/** @type {import('vite').UserConfig} */
+const config = {
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "src/variables.scss" as *;'
+			}
+		}
+	},
+	plugins: [sveltekit()]
+};
+
+export default config;
