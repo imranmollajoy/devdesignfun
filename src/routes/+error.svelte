c24d6d3ff303d759fc90b36cba0fc6e9cafd1d2f
Imran Molla Joy <imranmollajoy@gmail.com>
2023-04-30 10:40:54 +0600

    qol

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="error h-screen">
+	<h1>{$page.status}: {$page.error?.message}</h1>
+</div>
+
+<style>
+	.error {
+		display: grid;
+		place-content: center;
+	}
+</style>
