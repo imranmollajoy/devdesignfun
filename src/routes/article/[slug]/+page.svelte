8b1a1cd90467e5a430ae1b3dd42677edff380a9b
Imran Molla Joy <imranmollajoy@gmail.com>
2023-05-01 20:36:50 +0600

    more eyecandy

--- src/routes/article/[slug]/+page.svelte
+++ src/routes/article/[slug]/+page.svelte
@@ -22,24 +22,35 @@
 	/>
 {/if}
 <div class="container my-8">
-	<div class="grid grid-cols-3 gap-8">
-		<article class="col-span-3 lg:col-span-2 space-y-8">
-			<div class="flex gap-4 flex-wrap">
-				<a href="/category/{data.meta.category}">
-					<span class="badge {data.meta.category}">
-						{data.meta.category}
-					</span>
-				</a>
-				<p class="opacity-50">
-					{data.meta.readingTime} to read ~
+	<div class="space-y-4 my-16 mx-auto text-center">
+		<div class="flex gap-4 flex-wrap align-center justify-center">
+			<a href="/category/{data.meta.category}">
+				<span class="badge {data.meta.category}">
+					{data.meta.category}
+				</span>
+			</a>
+			<p class="opacity-50 space-x-4">
+				<span>
+					{data.meta.readingTime} to read
+				</span>
+				<span>
 					{getFormattedDate(data.meta.date)}
-				</p>
-			</div>
+				</span>
+			</p>
+		</div>
 
-			<h1>{data.meta.title}</h1>
-			{#if data.meta.cover}
-				<Img src="./{data.meta.slug}/{data.meta.cover?.image}" alt="" />
-			{/if}
+		<h1>{data.meta.title}</h1>
+		{#if data.meta.cover}
+			<img
+				src="./{data.meta.slug}/{data.meta.cover?.image}"
+				alt=""
+				loading="lazy"
+				class="max-w-96 mx-auto"
+			/>
+		{/if}
+	</div>
+	<div class="max-w-[1000px] mx-auto px-4">
+		<article class="col-span-3 lg:col-span-2 space-y-8">
 			<main class="prose-xl max-w-none">
 				<h3>Table of contents</h3>
 				<svelte:component this={data.content} />
@@ -59,24 +70,22 @@
 				</p>
 			{/if}
 		</article>
-		<div class="col col-span-3 lg:col-span-1 pb-32">
-			<div class="space-y-8 sticky top-32">
-				<!-- {#key data.content}
+		<div class="py-8">
+			<!-- {#key data.content}
 				{/key} -->
-				<div class="space-y-4">
-					<h3>Related</h3>
-					{#each data.related as post}
-						<article>
-							<p class="opacity-50">{post.date}</p>
-							<a href="/article/{post.slug}">
-								<h3>
-									{post.title}
-								</h3>
-								<p>{post.description}</p>
-							</a>
-						</article>
-					{/each}
-				</div>
+			<div class="space-y-4">
+				<h3>Related</h3>
+				{#each data.related as post}
+					<article>
+						<p class="opacity-50">{post.date}</p>
+						<a href="/article/{post.slug}">
+							<h3>
+								{post.title}
+							</h3>
+							<p>{post.description}</p>
+						</a>
+					</article>
+				{/each}
 			</div>
 		</div>
 	</div>
