235e27387705b00311623b618ecb4d3249780422
Imran Molla Joy <imranmollajoy@gmail.com>
2023-06-02 14:32:20 +0600

    indicator that clickable title

--- src/lib/components/Home/CategorizedColumn.svelte
+++ src/lib/components/Home/CategorizedColumn.svelte
@@ -8,7 +8,22 @@
 
 <div class="col my-4">
 	<a href="/category/{title}">
-		<h2 class="mb-4 text-{title}">{title}</h2>
+		<h2 class="mb-4 text-{title}">
+			{title}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				fill="currentColor"
+				viewBox="0 0 16 16"
+				class="inline"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+				/>
+			</svg>
+		</h2>
 	</a>
 	{#if posts.length !== 0}
 		{#each posts as post}
