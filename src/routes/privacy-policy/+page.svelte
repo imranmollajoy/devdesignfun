99bc7812edac0389c446d180137b36cf022e1831
Imran Molla Joy <imranmollajoy@gmail.com>
2023-06-03 16:47:15 +0600

    privacysettings

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -3,6 +3,32 @@
 </script>
 
 <div class="max-w-[1000px] mx-auto px-4 py-8">
+	<div
+		id="ppms_cm_privacy_settings"
+		class="ppms_cm_privacy_settings_widget"
+		data-editor-centralize="true"
+		data-main-container="true"
+		data-root="true"
+	>
+		<div class="ppms_cm_privacy_settings_widget_content" data-disable-select="true">
+			<h1
+				class="ppms_cm_privacy_settings_form_link_header"
+				id="ppms_cm_privacy_settings_form_link_header_id"
+			>
+				Privacy settings
+			</h1>
+			<p
+				class="ppms_cm_privacy_settings_form_link_text"
+				id="ppms_cm_privacy_settings_form_link_text_id"
+			>
+				We collect and process your data on this site to better understand how it is used. We always
+				ask you for consent to do that. You can change your privacy settings here.
+			</p>
+			<button class="ppms_cm_privacy_settings_button_show" id="ppms_cm_privacy_settings_button"
+				>Manage settings</button
+			>
+		</div>
+	</div>
 	<article class="prose-xl max-w-none">
 		<Content />
 	</article>
