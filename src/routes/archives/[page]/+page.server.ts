26e52599fb7f7a3c797211677cdfbd412d0fbc2f
Imran Molla Joy <imranmollajoy@gmail.com>
2024-01-13 21:36:37 +0600

    added limit

--- src/routes/archives/[page]/+page.server.ts
+++ src/routes/archives/[page]/+page.server.ts
@@ -6,15 +6,15 @@ export const load = async ({ url, params, fetch }) => {
 	const page = parseInt(params.page) || 1;
 
 	// Keeps from duplicationg the blog index route as page 1
-	if (page <= 1) {
-		redirect(301, '/archives');
+	if (page < 1) {
+		redirect(301, '/archives/1');
 	}
 
 	const offset = page * postsPerPage - postsPerPage;
 
 	const totalPostsRes = await fetch(`${url.origin}/api/posts/count`);
 	const { total, totalPages } = await totalPostsRes.json();
-	const posts = await fetchPosts(offset);
+	const posts = await fetchPosts(offset, postsPerPage);
 	const entry = {
 		start: offset + 1,
 		end: offset + 1 + posts.length - 1
