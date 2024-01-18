1a615e707f55c1870d38e8d70f922803aed5c3c2
Imran Molla Joy <imranmollajoy@gmail.com>
2024-01-18 12:46:03 +0600

    auto set target blank to external link

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	export let href: string;
+
+	function noTarget(href: string) {
+		return href.includes('localhost') || href.includes('devdesignfun.com');
+	}
 </script>
 
-<a {href} rel="noopener noreferrer">
-	<slot />
-</a>
+{#if noTarget(href)}
+	<a {href}>
+		<slot />
+	</a>
+{:else}
+	<a {href} target="_blank" rel="noopener noreferrer">
+		<slot />
+	</a>
+{/if}
