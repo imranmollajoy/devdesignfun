eaa3635b8c0a27de802a3d95be51ab46faa0f3e6
Imran Molla Joy <imranmollajoy@gmail.com>
2024-01-18 13:16:04 +0600

    links shenanigans

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
 	export let href: string;
 
-	function noTarget(href: string) {
-		return (
-			href.includes('localhost') || href.includes('devdesignfun.com') || href.charAt(0) === '#'
-		);
+	// Function to check if href is an external link
+	function isExternalLink(href) {
+		return href.startsWith('http') || href.startsWith('//');
 	}
 </script>
 
-{#if noTarget(href)}
-	<a {href}>
+{#if isExternalLink(href)}
+	<a {href} class="external-link text-secondary-500" target="_blank" rel="noopener noreferrer">
 		<slot />
 	</a>
 {:else}
-	<a {href} target="_blank" rel="noopener noreferrer">
+	<a {href}>
 		<slot />
 	</a>
 {/if}
+
+<style>
+	.external-link::after {
+		content: ' ↪';
+		font-size: 0.8em;
+		text-decoration: none !important;
+	}
+</style>
--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="max-w-[1000px] mx-auto px-4 py-8">
-	<article class="prose-xl max-w-none">
+	<article class="prose lg:prose-xl dark:prose-invert max-w-none">
 		<Content />
 	</article>
 </div>
