d63b9f9832f7544e4c5fd5a01f87984d65c9d45f
Imran Molla Joy <imranmollajoy@gmail.com>
2025-03-29 16:36:25 +0600

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,15 @@
 </script>
 
 <svelte:window bind:scrollY={y} />
-
+<svelte:head>
+	<link rel="icon" href="{base}/favicon.ico" />
+	<link rel="alternate" type="application/atom+xml" href="{base}/rss.xml" />
+	<link rel="sitemap" href="{base}/sitemap.xml" />
+	<link rel="apple-touch-icon" sizes="180x180" href="{base}/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="{base}/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="{base}/favicon-16x16.png" />
+	<link rel="manifest" href="{base}/site.webmanifest" />
+</svelte:head>
 <AppBar class="sticky top-0 z-10 {cls}" background="bg-surface-50-900-token ">
 	<svelte:fragment slot="lead">
 		<a href="{base}/">
